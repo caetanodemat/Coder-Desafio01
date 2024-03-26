1b6eb558bf5ece33bf64262c59e21eba87922996
Joao Pedro <joaopedrocaetanodematos@outlook.com>
2024-03-25 21:39:29 -0300

    Limpando o codigo

--- Desafio01/index1.js
+++ Desafio01/index1.js
@@ -1,11 +1,4 @@
 
-//var entrada = prompt("Insira uma letra")
-//var saida = entrada + " " + "info inserida"
-//alert(saida)
-
-//var valorString = prompt("Insira um numero:")
-//var valorConvertido = parseInt(valorString)
-//console.log(valorConvertido)
 
 var numeroUm = parseFloat(prompt("Insira o primeiro numero:"))
 var numeroDois = parseFloat(prompt("Insira o segundo numero:"))
